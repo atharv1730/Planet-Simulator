bf7d1ce27855e39e7ccd68f82c7d1d1ff788f153
Atharv <atharvkumar06@gmail.com>
2025-05-28 17:23:25 +0530

    draw function

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ pygame.display.set_caption("Planet Simulation")
 
 
 class Planet:
+    # Distance of earth from the sun in meters
+    AU = 149.6e6 * 1000  # Astronomical Unit in meters
+    G = 6.67428e-11  # Gravitational constant in m^3 kg^-1 s^-2
+    SCALE = 250 / AU  # 1 AU = 100 pixels
+    TIMESTEP = 3600 * 24  # One day
+    
+    
     def __init__(self, x, y, radius, color, mass):
         self.x = x
         self.y = y
@@ -26,7 +33,9 @@ class Planet:
         self.y_velocity = 0
 
     def draw(self, win):
-        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
 
 
 
