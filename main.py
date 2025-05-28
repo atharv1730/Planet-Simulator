46292f8510fb4ce4ed8518e67ec18120c7f2c09c
Atharv <atharvkumar06@gmail.com>
2025-05-28 17:11:37 +0530

    Main loop

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ WIDTH, HEIGHT = 800, 800
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    
+    pygame.quit()
+    
