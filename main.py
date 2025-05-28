e4bec952a89285d235794f0a23c1d99fa33288fb
Atharv <atharvkumar06@gmail.com>
2025-05-28 17:15:20 +0530

    Clock

--- main.py
+++ main.py
@@ -12,7 +12,16 @@ pygame.display.set_caption("Planet Simulation")
 
 def main():
     run = True
+    # Used to control the frame rate
+    clock = pygame.time.Clock()
+    
+    
     while run:
+        clock.tick(60)  # Limit to 60 frames per second
+        
+        win.fill((255, 255, 255))
+        pygame.display.update()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
