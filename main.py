9109ed5a5301dbe549cc6b4f09fe3b2b3ff1fa4e
Atharv <atharvkumar06@gmail.com>
2025-05-28 17:10:13 +0530

    Setting up the window

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import math
+
+# Initialize Pygame
+pygame.init()
+
+# Setting up the window
+WIDTH, HEIGHT = 800, 800
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Planet Simulation")
+
